471054b3c7b7c37b61b2038a65a3966f0690c17f
Hamza Shahid Siddiqui <hamzaasudani@gmail.com>
2025-12-03 12:41:39 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
     <div className="app-shell">
       <Header /> 
 
-      <main className="page p-4">
+      <main>
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
